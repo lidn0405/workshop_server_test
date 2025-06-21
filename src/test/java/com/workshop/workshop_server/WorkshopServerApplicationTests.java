4e848b434e7e90ff3934c276142c9b5b4c30e2df
Danny Li <lidn405@gmail.com>
2025-06-21 16:22:12 -0400

    workshop server test repo

---
+++ src/test/java/com/workshop/workshop_server/WorkshopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.workshop_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
